09f97f06c8afe55f5aaeaaef35e669d95b88cdae
varanustoken <varanustoken@gmail.com>
2023-05-31 08:59:54 +0700

    a

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -28,7 +28,7 @@
                     <a class="nav-link" href="#tokenomics">Tokenomics</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link bor" href="https://poocoin.app/tokens/0x">CHART</a>
+                    <a class="nav-link bor" href="https://poocoin.app/tokens/0xd09F81559cA6D438326eaf278c1298D9D3793C7D">CHART</a>
                 </li>
             </ul>
         </div>
